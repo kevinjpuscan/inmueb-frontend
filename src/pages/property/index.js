0d3c116d0538792276bc6f692bc61f93450c146a
kevin puscan <kevinjpuscan@gmail.com>
2020-07-27 22:47:51 -0500

    add map component

--- src/pages/property/index.js
+++ src/pages/property/index.js
@@ -5,6 +5,7 @@ import BtnGoBack from "../../components/btnGoBack";
 import BtnGoHome from "../../components/btnGoHome";
 import Wrapper from "../../components/wrapper";
 import CarouselImage from "../../components/carouselImage";
+import { Map, TileLayer, Polygon } from "react-leaflet";
 
 export const PropertyStyled = styled.div`
   .map {
@@ -15,20 +16,43 @@ export const PropertyStyled = styled.div`
   }
 
   .content-detail {
-    padding: 1.5em 0 90px 0;
+    padding: 1.5em 0 80px 0;
 
     h1 {
       font-size: 1.5em;
+      color: var(--gray-very-dark);
     }
 
     p {
       margin: 1em 0;
+      color: var(--gray-dark);
+    }
+  }
+
+  .features {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+    color: var(--primary);
+    font-weight: 900;
+    margin: 20px 0;
+    text-align: center;
+
+    div {
+      padding: 15px;
+      border-radius: 5px;
+      background: var(--gray-very-ligth);
+      text-align: center;
     }
   }
 `;
 
-function Property() {
-  let images = [
+let elementP = {
+  coin: "S/.",
+  mesure: "1000",
+  unit: "m2",
+  price: "150",
+  images: [
     {
       src:
         "https://a0.muscache.com/im/pictures/1ed971f7-ea85-4b5e-a486-17800ba91cb9.jpg?aki_policy=large",
@@ -49,7 +73,11 @@ function Property() {
         "https://a0.muscache.com/im/pictures/1ed971f7-ea85-4b5e-a486-17800ba91cb9.jpg?aki_policy=large",
       alt: "test"
     }
-  ];
+  ]
+};
+function Property({ element = elementP }) {
+  let { coin, price, mesure, unit, images } = element;
+
   return (
     <PropertyStyled>
       <Header
@@ -57,7 +85,31 @@ function Property() {
         center={<h1>Detalle</h1>}
         rigth={<BtnGoHome />}
       />
-      <div className="map"></div>
+      <div className="map">
+        <Map
+          center={[-7.1570213, -78.5173988]}
+          zoom={18}
+          maxZoom={50}
+          style={{ height: "100%" }}
+          zoomControl={false}
+        >
+          <TileLayer
+            url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+            subdomains={["mt0", "mt1", "mt2", "mt3"]}
+          />
+          <Polygon
+            positions={[
+              [
+                [-7.1573223, -78.5176998],
+                [-7.1573223, -78.5173987],
+                [-7.1570212, -78.5173987],
+                [-7.1570212, -78.5176998]
+              ]
+            ]}
+            color="var(--primary)"
+          ></Polygon>
+        </Map>
+      </div>
 
       <Wrapper>
         <div className="content-detail">
@@ -68,6 +120,20 @@ function Property() {
           </p>
           <div className="galery">
             <CarouselImage images={images} />
+          </div>
+          <div className="features">
+            <div>
+              <span>
+                <i className="fas fa-ruler-combined"></i> {mesure}
+                {unit}
+              </span>
+            </div>
+            <div>
+              <span>
+                <i className="fas fa-tag"></i> {coin}
+                {price}
+              </span>
+            </div>
           </div>
         </div>
       </Wrapper>
