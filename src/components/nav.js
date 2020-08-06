c9718c6e83995987d09cfaadfdb132cac1d02750
kevin puscan <kevinjpuscan@gmail.com>
2020-08-05 22:23:27 -0500

    add animation in nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Wrapper from "../components/wrapper";
 import NavList from "../components/navList";
@@ -10,20 +10,60 @@ export const NavStyled = styled.div`
   width: 100%;
   border-top: 1px solid var(--gray-ligth);
   position: fixed;
-  bottom: 0;
+  bottom: ${props => (props.isShow ? "0" : "-65px")};
   background: var(--white);
+  animation-duration: 0.3s;
+  animation-name: ${props => (props.isShow ? "show" : "hidden")};
+
+  @keyframes show {
+    from {
+      bottom: -65px;
+    }
+
+    to {
+      bottom: 0;
+    }
+  }
+  @keyframes hidden {
+    from {
+      bottom: 0px;
+    }
+
+    to {
+      bottom: -65px;
+    }
+  }
 `;
 
 function Nav({ isLogin }) {
   let location = useLocation();
+  let [isShow, setIsShow] = useState(false);
+  let [scroll, setScroll] = useState(0);
+
   const getNavForPath = () => {
     if (location.pathname.indexOf("property") > -1) {
       return <NavBuy />;
     }
     return <NavList isLogin={isLogin} />;
   };
+
+  useEffect(() => {
+    function onScroll() {
+      if (isShow && scroll < window.scrollY) {
+        setIsShow(false);
+      }
+      if (!isShow && scroll > window.scrollY) {
+        setIsShow(true);
+      }
+      setScroll(window.scrollY);
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [scroll, isShow]);
   return (
-    <NavStyled>
+    <NavStyled isShow={isShow}>
       <Wrapper>{getNavForPath()}</Wrapper>
     </NavStyled>
   );
